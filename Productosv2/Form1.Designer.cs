5857a1f7f50eac45c85b5c02b5fff2fe7ad07c4f
JARED ALEXANDER CASTILLO LACAYO <jared.castillo884@std.uni.edu.ni>
2024-11-25 17:57:04 -0600

    Agregar archivos de proyecto.

---
+++ Productosv2/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Productosv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtNombre = new TextBox();
+            txtPrecio = new TextBox();
+            txtCantidad = new TextBox();
+            btnAgregar = new Button();
+            btnMostrar = new Button();
+            btnVender = new Button();
+            listBoxProductos = new ListBox();
+            SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(232, 49);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(125, 27);
+            txtNombre.TabIndex = 0;
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(232, 172);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(125, 27);
+            txtPrecio.TabIndex = 1;
+            // 
+            // txtCantidad
+            // 
+            txtCantidad.Location = new Point(232, 307);
+            txtCantidad.Name = "txtCantidad";
+            txtCantidad.Size = new Size(125, 27);
+            txtCantidad.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(96, 463);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(94, 29);
+            btnAgregar.TabIndex = 3;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnMostrar
+            // 
+            btnMostrar.Location = new Point(232, 463);
+            btnMostrar.Name = "btnMostrar";
+            btnMostrar.Size = new Size(94, 29);
+            btnMostrar.TabIndex = 4;
+            btnMostrar.Text = "Mostrar";
+            btnMostrar.UseVisualStyleBackColor = true;
+            // 
+            // btnVender
+            // 
+            btnVender.Location = new Point(369, 463);
+            btnVender.Name = "btnVender";
+            btnVender.Size = new Size(94, 29);
+            btnVender.TabIndex = 5;
+            btnVender.Text = "Vender";
+            btnVender.UseVisualStyleBackColor = true;
+            // 
+            // listBoxProductos
+            // 
+            listBoxProductos.FormattingEnabled = true;
+            listBoxProductos.Location = new Point(722, 49);
+            listBoxProductos.Name = "listBoxProductos";
+            listBoxProductos.Size = new Size(370, 344);
+            listBoxProductos.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1185, 660);
+            Controls.Add(listBoxProductos);
+            Controls.Add(btnVender);
+            Controls.Add(btnMostrar);
+            Controls.Add(btnAgregar);
+            Controls.Add(txtCantidad);
+            Controls.Add(txtPrecio);
+            Controls.Add(txtNombre);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtNombre;
+        private TextBox txtPrecio;
+        private TextBox txtCantidad;
+        private Button btnAgregar;
+        private Button btnMostrar;
+        private Button btnVender;
+        private ListBox listBoxProductos;
+    }
+}
