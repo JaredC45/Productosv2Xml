3fc29d7e389b834e0d564c4b16c4c304c3d0cc01
JARED ALEXANDER CASTILLO LACAYO <jared.castillo884@std.uni.edu.ni>
2024-11-25 18:14:09 -0600

    Leer Productos

--- Productosv2/GestionProducto.cs
+++ Productosv2/GestionProducto.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Productosv2
 {
     public class GestionProducto
     {
         private const string archivoXml = "productos.xml";
+
+        public void GuardarProductos(List<Producto> productos)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Producto>));
+            using (StreamWriter writer = new StreamWriter(archivoXml))
+            {
+                serializer.Serialize(writer, productos);
+            }
+        }
+
+        public List<Producto> LeerProductos()
+        {
+            if (!File.Exists(archivoXml))
+                return new List<Producto>();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Producto>));
+            using (StreamReader reader = new StreamReader(archivoXml))
+            {
+                return (List<Producto>)serializer.Deserialize(reader);
+            }
+        }
     }
 }
