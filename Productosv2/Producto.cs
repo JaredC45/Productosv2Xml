82975f1b4efae6cc334113a544bda60bd2c4a261
JARED ALEXANDER CASTILLO LACAYO <jared.castillo884@std.uni.edu.ni>
2024-11-25 18:01:03 -0600

    Creacion de la clase producto

---
+++ Productosv2/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Productosv2
+{
+    [Serializable]
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
