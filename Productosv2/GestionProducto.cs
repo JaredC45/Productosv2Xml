5e00742742a07782bc951103e11022af6260aede
JARED ALEXANDER CASTILLO LACAYO <jared.castillo884@std.uni.edu.ni>
2024-11-25 18:04:33 -0600

    Creacion del nombre del archivo XML

---
+++ Productosv2/GestionProducto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productosv2
+{
+    public class GestionProducto
+    {
+        private const string archivoXml = "productos.xml";
+    }
+}
